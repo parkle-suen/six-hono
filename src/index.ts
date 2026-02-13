3e5c80e45bb6c88b2e5c13ea220cef5a0ab18702
richsun <25789351+parkle-suen@users.noreply.github.com>
2026-02-14 06:00:25 +0800

    ``` chore: 添加项目配置文件和文档
    添加 .gitignore 文件以忽略生产环境、开发环境、依赖项、环境变量、日志等文件
    
    添加 README.md 文件包含安装运行部署命令和类型生成说明
    ```

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+export default app
